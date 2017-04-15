a8c7ab91850bf70daea08fa901691097073a3971
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-15 02:31:38 -0400

    added endpoint to get all entries

--- server.js
+++ server.js
@@ -6,9 +6,23 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const knex = require('./utils/knex');
 
 app.use(bodyParser.text());
 
 server.listen(PORT, function () {
   console.log(`Peashooter listening on port ${PORT}!`);
+});
+
+/**
+ * Lists all the logs
+ */
+app.get('/list', function(req, res) {
+  knex('entries')
+  .select('*')
+  .then((entries) => {
+    res.status(200).send(entries);
+  }).catch((err) => {
+    sendError(res, err);
+  });
 });
