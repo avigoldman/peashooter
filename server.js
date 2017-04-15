ea65ad1fe774f92974385260781ef533724c211c
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-15 02:29:33 -0400

    added body parser

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+app.use(bodyParser.text());
 
 server.listen(PORT, function () {
   console.log(`Peashooter listening on port ${PORT}!`);
