df46f93ea393016bb5ffaea3927cb434cf56bbf2
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-15 03:16:00 -0400

    added nav and ability to link to tabs

--- public/dashboard.js
+++ public/dashboard.js
@@ -5,11 +5,28 @@ $(function() {
   $('.show-info').on('click', function(event) {
     $container.addClass('is-covered');
     $peashooterInfo.addClass('is-open');
+    linkToTab($(this).attr('href'));
   });
+
+  $('.navigation li a').on('click', function(event) {
+    linkToTab($(this).attr('href'));
+  });
+
 
   $('#peashooter-info__close').on('click', function(event) {
     event.preventDefault();
     $container.removeClass('is-covered');
     $peashooterInfo.removeClass('is-open');
   });
+
+  function linkToTab(target) {
+    var $link = $('.navigation [href="'+target+'"]');
+    var $target = $(target);
+
+    if ($target.length > 0) {
+      $('.tabs .tab, .navigation li a').removeClass('is-active');
+      $link.addClass('is-active');
+      $target.addClass('is-active');
+    }
+  }
 });
