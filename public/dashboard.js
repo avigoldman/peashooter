fda5cf47d563edafeae9b15b87406e4eab4eac5e
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-15 03:12:00 -0400

    added all the nav and js to open overlay

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,4 +1,9 @@
 $(function() {
   var $container = $('#container');
   var $peashooterInfo = $('#peashooter-info');
+
+  $('.show-info').on('click', function(event) {
+    $container.addClass('is-covered');
+    $peashooterInfo.addClass('is-open');
+  });
 });
